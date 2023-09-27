72da0b5877b331600af4fc923360693704861ea3
mattdags982 <mattd@wizehire.com>
2023-09-27 17:53:42 -0400

    initial frontend set up

---
+++ src/next/app/page.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+
+const story = {
+  title: "The Gourmet Dilemma",
+  description:
+    "A man tries to order food at a restaurant, but the conversation takes some unexpected twists.",
+  conversations: [
+    {
+      name: "Waitress",
+      contentEnglish:
+        "Good evening, sir. Would you like to start with something to drink?",
+      contentSpanish:
+        "Buenas noches, señor. ¿Le gustaría empezar con algo para beber?",
+    },
+    {
+      name: "Man",
+      contentEnglish: "Hmm, do you serve organic, gluten-free water?",
+      contentSpanish: "Hmm, ¿sirven agua orgánica sin gluten?",
+    },
+    {
+      name: "Waitress",
+      contentEnglish: "Uh, water is naturally gluten-free, sir.",
+      contentSpanish: "Eh, el agua es naturalmente sin gluten, señor.",
+    },
+    {
+      name: "Man",
+      contentEnglish: "Excellent! I'll take a glass of that exquisite water.",
+      contentSpanish: "¡Excelente! Tomaré un vaso de esa agua exquisita.",
+    },
+    {
+      name: "Waitress",
+      contentEnglish: "Very well. Are you ready to order food?",
+      contentSpanish: "Muy bien. ¿Está listo para ordenar comida?",
+    },
+    {
+      name: "Man",
+      contentEnglish: "Yes, can I have a vegan steak?",
+      contentSpanish: "Sí, ¿puedo tener un bistec vegano?",
+    },
+    {
+      name: "Waitress",
+      contentEnglish: "I'm afraid we only serve real steak, sir.",
+      contentSpanish: "Me temo que solo servimos bistec real, señor.",
+    },
+    {
+      name: "Man",
+      contentEnglish:
+        "Ah, too bad. Alright, I'll go for the chicken parmesan then.",
+      contentSpanish:
+        "Ah, qué mal. De acuerdo, entonces tomaré el pollo a la parmesana.",
+    },
+    {
+      name: "Waitress",
+      contentEnglish:
+        "Would you like that cooked, or should I just bring out a chicken and some cheese?",
+      contentSpanish:
+        "¿Le gustaría eso cocido, o debo traer solo un pollo y un poco de queso?",
+    },
+    {
+      name: "Man",
+      contentEnglish: "Ha! You've got a sense of humor. Cooked, please!",
+      contentSpanish: "¡Ja! Tienes sentido del humor. ¡Cocido, por favor!",
+    },
+    {
+      name: "Waitress",
+      contentEnglish: "Of course, sir. Anything else?",
+      contentSpanish: "Por supuesto, señor. ¿Algo más?",
+    },
+    {
+      name: "Man",
+      contentEnglish:
+        "A sense of purpose in life. But for now, the chicken will do.",
+      contentSpanish:
+        "Un sentido de propósito en la vida. Pero por ahora, el pollo estará bien.",
+    },
+  ],
+};
+export default function StoriesDashboard() {
+  return (
+    <div className="h-full w-full bg-white">
+      <div>{story.title}</div>
+      <div>{story.description}</div>
+      <div>
+        {story.conversations.map((blurb) => (
+          <div key={blurb.contentEnglish}>
+            <div>{blurb.name}</div>
+            <div>{blurb.contentEnglish}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
