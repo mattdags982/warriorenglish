bd197cbb39354e8300799824021c282394bbe889
mattdags982 <mattd@wizehire.com>
2023-09-28 17:59:59 -0400

    set up django rest api

--- src/django/app/settings.py
+++ src/django/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django Rest Framework
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
--- src/django/app/urls.py
+++ src/django/app/urls.py
@@ -14,10 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
+
 from django.contrib import admin
 from django.urls import include, path
+from stories import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path("admin/", admin.site.urls),
+    # path("", include("blog.urls")),
+    path("", include(router.urls)),
+    # path("api/", include("stories_api.urls", namespace="stories_api")),
+    # If you're intending to use the browsable API you'll probably also want to add REST framework's login and logout views
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
