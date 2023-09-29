8294a72ad7803719de81bb4ffd3a12d0575c7412
mattdags982 <mattd@wizehire.com>
2023-09-28 21:38:30 -0400

    update story serializers with new data model

--- src/django/stories/serializers.py
+++ src/django/stories/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Story
+from .models import Conversation, Story, Translation
 
 # In Django Rest Framework (DRF), a serializer is a component that allows you to convert
 # complex data types, such as Django models, into Python data types that can be easily
@@ -9,7 +9,31 @@ from .models import Story
 # or received as a payload from a client.
 
 
+class TranslationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Translation
+        fields = ["language_code", "translated_content"]
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    translations = TranslationSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ["sequence", "character_name", "content_english", "translations"]
+
+
 class StorySerializer(serializers.ModelSerializer):
+    conversations = ConversationSerializer(many=True, read_only=True)
+
     class Meta:
         model = Story
-        fields = ["id", "title", "description", "content"]
+        fields = [
+            "id",
+            "title",
+            "description",
+            "audio_link",
+            "created_at",
+            "updated_at",
+            "conversations",
+        ]
