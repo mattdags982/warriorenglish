ac5a533f9628e7d7e261336ed1d565ddfd458e99
mattdags982 <mattd@wizehire.com>
2023-09-28 20:43:12 -0400

    set up stories model

--- src/django/stories/models.py
+++ src/django/stories/models.py
@@ -2,9 +2,49 @@ from django.db import models
 
 
 class Story(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    content = models.TextField()
+    audio_link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class Conversation(models.Model):
+    story = models.ForeignKey(
+        Story, related_name="conversations", on_delete=models.CASCADE
+    )
+    sequence = models.IntegerField()
+    character_name = models.CharField(max_length=255)
+    content_english = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # means that by default, whenever you retrieve Conversations for a Story,
+        # they will be ordered by the sequence field in ascending order.
+        ordering = ["sequence"]
+
+    def __str__(self):
+        return f"{self.character_name} - {self.content_english}"
+
+
+class Translation(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, related_name="translations", on_delete=models.CASCADE
+    )
+    language_code = models.CharField(max_length=10)
+    translated_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # ensures that you can’t have multiple Translation objects with the same
+        # conversation and language_code. This means that for each Conversation,
+        # there can only be one Translation per language_code
+        unique_together = ["conversation", "language_code"]
+
+    def __str__(self):
+        return f"{self.language_code} - {self.translated_content}"
